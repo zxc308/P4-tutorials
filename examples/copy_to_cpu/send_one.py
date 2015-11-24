5c987569d117d7a5772310fa7d6d65cf87620342
Tu Dang <danghu@usi.ch>
2015-11-24 09:21:58 +0000

    send_one.py: set all bits in ethernet header to 0

--- examples/copy_to_cpu/send_one.py
+++ examples/copy_to_cpu/send_one.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-p = Ether(dst="aa:bb:cc:dd:ee:ff") / IP(dst="10.0.1.10") / TCP() / "aaaaaaaaaaaaaaaaaaa"
-# p.show()
+p = Ether(dst="00:00:00:00:00:00", src="00:00:00:00:00:00", type=0x0) / IP(dst="10.0.1.10") / TCP() / "aaaaaaaaaaaaaaaaaaa"
+p.show()
 hexdump(p)
 sendp(p, iface = "veth1")
