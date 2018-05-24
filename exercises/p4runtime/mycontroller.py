0375f0e6890a60f448c0019e40e2e626c781e4a4
Brian O'Connor <bocon@opennetworking.org>
2018-05-24 00:17:48 +0000

    Fixing Ctrl-C hang by shutdown switches

--- exercises/p4runtime/mycontroller.py
+++ exercises/p4runtime/mycontroller.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import sys
+import grpc
 from time import sleep
 
 # Import P4Runtime lib from parent utils dir
@@ -10,6 +11,7 @@ sys.path.append(
     os.path.join(os.path.dirname(os.path.abspath(__file__)),
                  '../../utils/'))
 import p4runtime_lib.bmv2
+from p4runtime_lib.switch import ShutdownAllSwitches
 import p4runtime_lib.helper
 
 SWITCH_TO_HOST_PORT = 1
@@ -128,47 +130,47 @@ def main(p4info_file_path, bmv2_file_path):
     # Instantiate a P4 Runtime helper from the p4info file
     p4info_helper = p4runtime_lib.helper.P4InfoHelper(p4info_file_path)
 
-    # Create a switch connection object for s1 and s2;
-    # this is backed by a P4 Runtime gRPC connection.
-    # Also, dump all P4Runtime messages sent to switch to given txt files.
-    s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
-        name='s1',
-        address='127.0.0.1:50051',
-        device_id=0,
-        proto_dump_file='logs/s1-p4runtime-requests.txt')
-    s2 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
-        name='s2',
-        address='127.0.0.1:50052',
-        device_id=1,
-        proto_dump_file='logs/s2-p4runtime-requests.txt')
-
-    # Send master arbitration update message to establish this controller as
-    # master (required by P4Runtime before performing any other write operation)
-    s1.MasterArbitrationUpdate()
-    s2.MasterArbitrationUpdate()
-
-    # Install the P4 program on the switches
-    s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
-                                   bmv2_json_file_path=bmv2_file_path)
-    print "Installed P4 Program using SetForwardingPipelineConfig on s1"
-    s2.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
-                                   bmv2_json_file_path=bmv2_file_path)
-    print "Installed P4 Program using SetForwardingPipelineConfig on s2"
-
-    # Write the rules that tunnel traffic from h1 to h2
-    writeTunnelRules(p4info_helper, ingress_sw=s1, egress_sw=s2, tunnel_id=100,
-                     dst_eth_addr="00:00:00:00:02:02", dst_ip_addr="10.0.2.2")
-
-    # Write the rules that tunnel traffic from h2 to h1
-    writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200,
-                     dst_eth_addr="00:00:00:00:01:01", dst_ip_addr="10.0.1.1")
-
-    # TODO Uncomment the following two lines to read table entries from s1 and s2
-    # readTableRules(p4info_helper, s1)
-    # readTableRules(p4info_helper, s2)
-
-    # Print the tunnel counters every 2 seconds
     try:
+        # Create a switch connection object for s1 and s2;
+        # this is backed by a P4 Runtime gRPC connection.
+        # Also, dump all P4Runtime messages sent to switch to given txt files.
+        s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+            name='s1',
+            address='127.0.0.1:50051',
+            device_id=0,
+            proto_dump_file='logs/s1-p4runtime-requests.txt')
+        s2 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+            name='s2',
+            address='127.0.0.1:50052',
+            device_id=1,
+            proto_dump_file='logs/s2-p4runtime-requests.txt')
+
+        # Send master arbitration update message to establish this controller as
+        # master (required by P4Runtime before performing any other write operation)
+        s1.MasterArbitrationUpdate()
+        s2.MasterArbitrationUpdate()
+
+        # Install the P4 program on the switches
+        s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
+                                       bmv2_json_file_path=bmv2_file_path)
+        print "Installed P4 Program using SetForwardingPipelineConfig on s1"
+        s2.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
+                                       bmv2_json_file_path=bmv2_file_path)
+        print "Installed P4 Program using SetForwardingPipelineConfig on s2"
+
+        # Write the rules that tunnel traffic from h1 to h2
+        writeTunnelRules(p4info_helper, ingress_sw=s1, egress_sw=s2, tunnel_id=100,
+                         dst_eth_addr="00:00:00:00:02:02", dst_ip_addr="10.0.2.2")
+
+        # Write the rules that tunnel traffic from h2 to h1
+        writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200,
+                         dst_eth_addr="00:00:00:00:01:01", dst_ip_addr="10.0.1.1")
+
+        # TODO Uncomment the following two lines to read table entries from s1 and s2
+        # readTableRules(p4info_helper, s1)
+        # readTableRules(p4info_helper, s2)
+
+        # Print the tunnel counters every 2 seconds
         while True:
             sleep(2)
             print '\n----- Reading tunnel counters -----'
@@ -179,6 +181,14 @@ def main(p4info_file_path, bmv2_file_path):
     except KeyboardInterrupt:
         print " Shutting down."
 
+    except grpc.RpcError as e:
+        print "gRPC Error:", e.details(),
+        status_code = e.code()
+        print "(%s)" % status_code.name,
+        traceback = sys.exc_info()[2]
+        print "[%s:%d]" % (traceback.tb_frame.f_code.co_filename, traceback.tb_lineno)
+
+    ShutdownAllSwitches()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='P4Runtime Controller')
@@ -198,5 +208,4 @@ if __name__ == '__main__':
         parser.print_help()
         print "\nBMv2 JSON file not found: %s\nHave you run 'make'?" % args.bmv2_json
         parser.exit(1)
-
     main(args.p4info, args.bmv2_json)
