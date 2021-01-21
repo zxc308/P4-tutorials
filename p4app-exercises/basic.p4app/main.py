dc19d69488b2f7420901769d5ce7d17050a96ef5
Theo <theojepsen@g-m-a-i-l-dotcom>
2021-01-21 11:01:07 +0000

    migrated exercises to Python3

--- p4app-exercises/basic.p4app/main.py
+++ p4app-exercises/basic.p4app/main.py
@@ -34,7 +34,7 @@ class RingTopo(Topo):
 
         switches = []
 
-        for i in xrange(1, n+1):
+        for i in range(1, n+1):
             host = self.addHost('h%d' % i,
                                 ip = hostIP(i),
                                 mac = hostMAC(i))
@@ -43,7 +43,7 @@ class RingTopo(Topo):
             switches.append(switch)
 
         # Port 2 connects to the next switch in the ring, and port 3 to the previous
-        for i in xrange(n):
+        for i in range(n):
             self.addLink(switches[i], switches[(i+1)%n], port1=2, port2=3)
 
 topo = RingTopo(N)
