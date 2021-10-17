b65cd594cfbfa938fb1792cb6cddd368b651fe16
Mehmet Firat Aksoy <m.firat.aksoy@tcu.edu>
2021-10-16 22:27:13 -0500

    Installation Fix
    Handshake request fails at https://download.sublimetext.com/ apt/stable/" during the installation, making the whole repository not work for new developers. This fix solves the issue. This is an important fix.

--- vm-ubuntu-20.04/root-bootstrap.sh
+++ vm-ubuntu-20.04/root-bootstrap.sh
@@ -9,9 +9,9 @@ set -xe
 # commands.  sudo is unnecessary here since this entire script is
 # executed as the user root.
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+wget -qO - http://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb http://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
 # These commands are done later below
 #apt-get update
 #apt-get install sublime-text
