4b9cc36a49c47bb59d1d02cd93dbe22aa8d2799f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-04-09 17:50:08 -0400

    Specify a particular recent commit of Mininet to use

--- vm-ubuntu-20.04/user-common-bootstrap.sh
+++ vm-ubuntu-20.04/user-common-bootstrap.sh
@@ -4,8 +4,10 @@
 set -xe
 
 # --- Mininet --- #
+MININET_COMMIT="aa0176fce6fb718a03474f8719261b07b670d30d"  # 2022-Apr-02
 git clone https://github.com/mininet/mininet mininet
 cd mininet
+git checkout ${MININET_COMMIT}
 PATCH_DIR="${HOME}/patches"
 patch -p1 < "${PATCH_DIR}/mininet-dont-install-python2-2022-apr.patch" || echo "Errors while attempting to patch mininet, but continuing anyway ..."
 cd ..
