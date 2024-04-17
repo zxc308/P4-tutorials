9afd74d334b94ce8d99e8c2bb0cd5aed3031a6c5
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2024-04-17 22:13:12 +0530

    Ipv6 address fixes

--- utils/p4runtime_lib/convert.py
+++ utils/p4runtime_lib/convert.py
@@ -140,7 +140,7 @@ if __name__ == '__main__':
     assert(not matchesIPv4('10001'))
 
     assert(matchesIPv6('2001:0db8:85a3:0000:0000:8a2e:0370:7334'))
-    assert(not matchesIPv6('2001:0db8:85a3:0000:0000:8a2e:0370'))
+    assert(not matchesIPv6('241.54.113.65'))
     assert(not matchesIPv6('::1::2'))
     assert(not matchesIPv6('241.54.113.65'))
 
