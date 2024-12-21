7ab0a39efea84c672a8bf2ac43242f83d22b7f56
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-12-21 00:51:53 -0500

    Delete unused dirs, and configure some settings, in user-bootstrap.sh
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- vm-ubuntu-24.04/user-bootstrap.sh
+++ vm-ubuntu-24.04/user-bootstrap.sh
@@ -26,3 +26,44 @@ cd ~
 sudo mv .vim $HOME/.vim
 sudo chown -R $USER:$USER $HOME/.vim
 sudo chown $USER:$USER $HOME/.vimrc
+
+# Remove unused directories, if they exist.
+for dirname in Documents Music Pictures Public Templates Videos
+do
+    if [ -d $HOME/$dirname ]
+    then
+	rmdir $HOME/$dirname
+    fi
+done
+
+# Update settings via the following corresponding gsettings commands.
+
+# This command has the same effect as doing this in Settings app
+# in Ubuntu 24.04:
+# Privacy & Security -> Screen Lock -> Blank Screen Delay -> Never
+gsettings set org.gnome.desktop.session idle-delay 0
+
+# Privacy & Security -> Screen Lock -> Automatic Screen Lock -> disabled
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+# Ubuntu Desktop -> Dock -> Icon Size -> 24
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+
+# Update the list of icons pinned in the Dash:
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Settings.desktop', 'update-manager.desktop', 'yelp.desktop']"
+
+# System -> Users -> user account "p4" -> Automatic Login -> enabled
+# Changing this does not change the output of the command:
+# gsettings list-recursively
+# It must modify something on the system elsewhere.
+# I learned that it causes a line like the following in file
+# /etc/gdm3/custom.conf to have the value after the = change from False to True:
+#AutomaticLoginEnable=False
+# This line is in a section labeled [daemon].
+# It also causes this line to be created, or its value modified,
+# to the user account name:
+#AutomaticLogin=p4
+
+# TODO: The best way to change this from a Bash script would be to
+# find a well-tested utility program that modifies or adds settings to
+# files with that format, which is like a Windows INI format file.
