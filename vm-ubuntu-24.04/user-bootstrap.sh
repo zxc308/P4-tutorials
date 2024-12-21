626a5b8c703c34bc5712a7a85a0aab4121d88793
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-12-21 02:48:33 -0500

    Updates to user-bootstrap.sh
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- vm-ubuntu-24.04/user-bootstrap.sh
+++ vm-ubuntu-24.04/user-bootstrap.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
+# Remember the current directory when the script was started:
+INSTALL_DIR="${PWD}"
+
+THIS_SCRIPT_FILE_MAYBE_RELATIVE="$0"
+THIS_SCRIPT_DIR_MAYBE_RELATIVE="${THIS_SCRIPT_FILE_MAYBE_RELATIVE%/*}"
+THIS_SCRIPT_DIR_ABSOLUTE=`readlink -f "${THIS_SCRIPT_DIR_MAYBE_RELATIVE}"`
+
 # Print script commands and exit on errors.
 set -xe
 
 # --- Emacs --- #
-sudo cp p4_16-mode.el /usr/share/emacs/site-lisp/
-sudo mkdir $HOME/.emacs.d/
+cp ${THIS_SCRIPT_DIR_ABSOLUTE}/p4_16-mode.el /usr/share/emacs/site-lisp/
+mkdir $HOME/.emacs.d/
 echo "(autoload 'p4_16-mode' \"p4_16-mode.el\" \"P4 Syntax.\" t)" > init.el
 echo "(add-to-list 'auto-mode-alist '(\"\\.p4\\'\" . p4_16-mode))" | tee -a init.el
-sudo mv init.el $HOME/.emacs.d/
-sudo ln -s /usr/share/emacs/site-lisp/p4_16-mode.el $HOME/.emacs.d/p4_16-mode.el
-sudo chown -R $USER:$USER $HOME/.emacs.d/
+mv init.el $HOME/.emacs.d/
+ln -s /usr/share/emacs/site-lisp/p4_16-mode.el $HOME/.emacs.d/p4_16-mode.el
 
 # --- Vim --- #
 cd ~
-mkdir .vim
+mkdir -p .vim
 cd .vim
 mkdir ftdetect
 mkdir syntax
 echo "au BufRead,BufNewFile *.p4      set filetype=p4" >> ftdetect/p4.vim
 echo "set bg=dark" >> ~/.vimrc
-sudo mv ~/.vimrc $HOME/.vimrc
-cp ~/p4.vim syntax/p4.vim
-cd ~
-sudo mv .vim $HOME/.vim
-sudo chown -R $USER:$USER $HOME/.vim
-sudo chown $USER:$USER $HOME/.vimrc
+cp ${THIS_SCRIPT_DIR_ABSOLUTE}/p4.vim syntax/p4.vim
 
 # Remove unused directories, if they exist.
 for dirname in Documents Music Pictures Public Templates Videos
