9fe83db28de364d533c09cd586b7ce15afdaa4d3
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-12-21 00:35:25 -0500

    Small updates to user-bootstrap.sh
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- vm-ubuntu-24.04/user-bootstrap.sh
+++ vm-ubuntu-24.04/user-bootstrap.sh
@@ -5,12 +5,12 @@ set -xe
 
 # --- Emacs --- #
 sudo cp p4_16-mode.el /usr/share/emacs/site-lisp/
-sudo mkdir /home/p4/.emacs.d/
+sudo mkdir $HOME/.emacs.d/
 echo "(autoload 'p4_16-mode' \"p4_16-mode.el\" \"P4 Syntax.\" t)" > init.el
 echo "(add-to-list 'auto-mode-alist '(\"\\.p4\\'\" . p4_16-mode))" | tee -a init.el
-sudo mv init.el /home/p4/.emacs.d/
-sudo ln -s /usr/share/emacs/site-lisp/p4_16-mode.el /home/p4/.emacs.d/p4_16-mode.el
-sudo chown -R p4:p4 /home/p4/.emacs.d/
+sudo mv init.el $HOME/.emacs.d/
+sudo ln -s /usr/share/emacs/site-lisp/p4_16-mode.el $HOME/.emacs.d/p4_16-mode.el
+sudo chown -R $USER:$USER $HOME/.emacs.d/
 
 # --- Vim --- #
 cd ~
@@ -20,9 +20,9 @@ mkdir ftdetect
 mkdir syntax
 echo "au BufRead,BufNewFile *.p4      set filetype=p4" >> ftdetect/p4.vim
 echo "set bg=dark" >> ~/.vimrc
-sudo mv ~/.vimrc /home/p4/.vimrc
+sudo mv ~/.vimrc $HOME/.vimrc
 cp ~/p4.vim syntax/p4.vim
 cd ~
-sudo mv .vim /home/p4/.vim
-sudo chown -R p4:p4 /home/p4/.vim
-sudo chown p4:p4 /home/p4/.vimrc
+sudo mv .vim $HOME/.vim
+sudo chown -R $USER:$USER $HOME/.vim
+sudo chown $USER:$USER $HOME/.vimrc
